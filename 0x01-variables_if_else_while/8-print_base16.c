b2c1ddeacef2fb6c07abb8cce144b74f8f10f18a
ridhwan497 <ridubangire@gmail.com>
2023-03-18 02:49:15 +0300

    ...

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+*main - main function
+*Description: program prints numbers of base 16 in lowercase with putchar 
+*Return: 0
+*/
+
+int main (void)
+{
+	int num; /*this is the numbers 0-9*/
+	char c; /*this is for the letters a-f*/
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
