5fb20eab6dacde284650d8aa13224839eb1fd1f9
ridhwan497 <ridubangire@gmail.com>
2023-03-18 02:03:59 +0300

    ...

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,17 @@
 */
 int main(void)
 {
-    char lc;
-    char uc;
+	char lc;
+	char uc;
 
-    for (lc = 'a' ; lc <= 'z'; lc++)
-    {
-        putchar(lc);
-    }
-    for (uc = 'A' ; uc <= 'Z'; uc++)
-    {
-        putchar(uc);
-    }
-    printf("\n");
-    return (0);
+	for (lc = 'a' ; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for (uc = 'A' ; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	printf("\n");
+	return (0);
 }
