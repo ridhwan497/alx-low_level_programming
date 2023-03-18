1fb1b085632a8e1605fdb0ec9d61af8b88731c26
ridhwan497 <ridubangire@gmail.com>
2023-03-18 03:35:42 +0300

    wah advanced nayo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,13 +3,15 @@
 
 /**
 *main - main function
-*Description - program prints all possible different combinations of three digits
+*Description - program prints all possible different combinations of 3 digits
 *Return: 0
 */
 
 int main(void)
 {
-	int num1, num2, num3; /*this variable is for the first, second and third digits*/
+	int num1; /*this variable is for the first digit*/
+	int num2; /*this variable is for the second digit*/
+	int num3; /*this variable is for the third digit*/
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
