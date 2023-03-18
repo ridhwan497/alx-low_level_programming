6fd6cb53bb1e8de8982c9ac249beff3098af09a2
ridhwan497 <ridubangire@gmail.com>
2023-03-18 03:51:11 +0300

    wah advanced nayo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
 /**
 *main - main function
 *Description: program prints all possible combinations of two two-digit numbers
-*Return: 0 
+*Return: 0
 */
 
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for(num2 = 0; num2 < 10; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			for (num3 = 0; num3 < 10; num3++)
 			{
