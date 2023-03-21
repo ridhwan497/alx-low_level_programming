c45823c37a314eefbdecf3d7534aaa084353e393
ridhwan497 <ridubangire@gmail.com>
2023-03-22 00:45:09 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isalpha - return 1 if c is a letter, 0 if not
+* @c: char letter
+*Return: 1 if letter, 0 if not
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
