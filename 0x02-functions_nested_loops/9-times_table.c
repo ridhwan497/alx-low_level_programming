ac47b686f54595fa81d572405f483cf85cc63fdd
ridhwan497 <ridubangire@gmail.com>
2023-03-22 02:28:22 +0300

    ...

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the n times table, starting with 0
+ * Return: void
+ */
+void print_times_table(void)
+{
+	int i, j, k; /* i = row, j = column, k = product */
+
+	for (i = 0; i < 10; i++) /* 10 rows */
+	{
+		for (j = 0; j < 10; j++) /* 10 columns */
+		{
+			k = i * j; /* product */
+			if (k < 10) /* single digit */
+			{
+				if (j != 0) /* not first column */
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(k + '0'); /* print product */
+			}
+			else if (k < 100) /* double digit */
+			{
+				_putchar(' '); /* print space */
+				_putchar((k / 10) + '0');/* print tens */
+				_putchar((k % 10) + '0');/* print ones */
+			}
+			else /* triple digit */
+			{
+				_putchar((k / 100) + '0');/* print hundreds */
+				_putchar(((k / 10) % 10) + '0');/* print tens */
+				_putchar((k % 10) + '0');/* print ones */
+			}
+			if (j != 9) /*not last column*/
+			{
+				_putchar(','); /* print comma */
+				_putchar(' '); /* print space */
+			}
+		}
+		_putchar('\n');
+	}
+}
