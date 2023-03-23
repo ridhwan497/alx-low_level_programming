bffb42e202fe5d095ea1a0930c1e590418702956
ridhwan497 <ridubangire@gmail.com>
2023-03-24 01:04:52 +0300

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,9 +11,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-int main(void);
 void print_triangle(int size);
-int main(void);
-void print_number(int n)
+void print_number(int n);
 
 #endif
