46f8ed8dcb786ea9f0f5bafda62fdc6f1ba814b6
ridhwan497 <ridubangire@gmail.com>
2023-03-23 12:25:10 +0300

    tried recreating the isupper function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,5 +21,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-int main(void);
 #endif
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * 
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x04-more_functions_nested_loops
+ */
+int _isupper(int c);
+
+#endif
