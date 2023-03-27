2d37b5547a87162bf415f3dab8be48536e9ac1aa
ridhwan497 <ridubangire@gmail.com>
2023-03-27 20:55:13 +0300

    reduced characters in description to 80

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *reset_to_98 - takes a pointer to an int &updates the value it points to to 98.
+ *reset_to_98 - takes a pointer to an int &updates the value it points to 98.
  *@n: pointer to an int
  * Return: Always 0.
  */
