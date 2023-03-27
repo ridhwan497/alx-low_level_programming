6cef550fda19f3e996d005ec6a417a1cd3e9cfe1
ridhwan497 <ridubangire@gmail.com>
2023-03-27 22:06:51 +0300

    adding the function int _putchar(char c) to slve the error error: implicit declaration of function ‘_putchar’

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * Desc: Header file containing declarations for all functions
  *       used in the 0x05-pointers_arrays_strings
  */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
