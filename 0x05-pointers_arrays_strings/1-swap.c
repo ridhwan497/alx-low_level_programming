309a1ea35cf2e23840abde470553b798584c2fbd
ridhwan497 <ridubangire@gmail.com>
2023-03-27 21:05:36 +0300

    created 1-swap.c that swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of 2 integers.
+ * @a: pointer to int
+ * @b: pointer to int
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
