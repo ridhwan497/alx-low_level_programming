b4af12eb98ea7c327f79b5b5b589a5791085308b
ridhwan497 <ridubangire@gmail.com>
2023-03-27 20:50:06 +0300

    removed trailing whitespace 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ *reset_to_98 - takes a pointer to an int &updates the value it points to to 98.
  *@n: pointer to an int
- * Return: Always 0.  
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
