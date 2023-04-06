3ef4ec5554a6787a1ca23ad85e94e198eb92a70e
ridhwan497 <ridubangire@gmail.com>
2023-04-06 04:24:58 +0300

    function to determine if a number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * primey - checks if a number is prime
+ * @num: integer number
+ * @check: integer number
+ * Return: 1 if prime, otherwise 0
+ */
+
+int primey(int num, int check)
+{
+	if (num == check)
+	{
+		return (1);
+	}
+	else if (num % check == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primey(num, check + 1));
+	}
+}
+
+/**
  * is_prime_number - returns 1 if the input integer is a prime number
  * @n: integer number
  * Return: 1 if n is a prime number, otherwise 0
@@ -8,18 +31,9 @@
 
 int is_prime_number(int n)
 {
-	int counter;
-
-	counter = 2; /* 2 is the first non negative prime int*/
-
-	if (n <= 1)
+	if (n == 0 || n < 0 || n == 1)
 	{
 		return (0);
 	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-
-	return (counter * counter);
+	return (primey(n, 2));
 }
