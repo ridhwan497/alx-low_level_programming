b26964aa5cffad314d0ae6a16b0e8dcdc320445b
ridhwan497 <ridubangire@gmail.com>
2023-04-09 22:01:10 +0300

    \n

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - prints its name, followed by a new line.
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0.
+ * Return: 0 always.
  */
 int main(int argc __attribute__((unused)), char *argv[]) /*argc is unused to prevent warnings*/
 {
