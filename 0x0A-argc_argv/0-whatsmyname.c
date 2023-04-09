2f054fa00dc1333dc74018c4645c711c85c791f7
ridhwan497 <ridubangire@gmail.com>
2023-04-09 22:04:45 +0300

    \n

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
  * @argv: argument vector
  * Return: 0 always.
  */
-int main(int argc __attribute__((unused)), char *argv[]) /*argc is unused to prevent warnings*/
+int main(int argc __attribute__((unused)), char *argv[])
+/*argc is unused to prevent warnings*/
 {
 	printf("%s\n", argv[0]);
 	return (0);
