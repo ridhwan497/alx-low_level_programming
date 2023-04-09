81d40901a68cb2c96b78b81f907daf2a8980858b
ridhwan497 <ridubangire@gmail.com>
2023-04-09 22:25:39 +0300

    as usual :)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - prints the sum of positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-	int nums, sum, i;
+	int sum, i;
 
 	sum = 0;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
-	else 
+	else
 	{
-		for (i =1; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (isdigit(*argv[i]))
 			{
-				nums = atoi(argv[i]);
-				sum += nums;
+				sum += atoi(argv[i]);
 			}
 			else
 			{
