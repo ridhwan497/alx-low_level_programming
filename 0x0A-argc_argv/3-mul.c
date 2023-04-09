a1204331610ce86dd0ef83a782e0a6e221e54286
ridhwan497 <ridubangire@gmail.com>
2023-04-09 22:21:24 +0300

    what is it about spaces and newlines

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h> /*atoi is defined here!*/
 
+/**
+ * main - prints the product of two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int num1, num2, mul;
 
 	mul = 0;
 
-	if (argc ==3)
+	if (argc == 3)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
