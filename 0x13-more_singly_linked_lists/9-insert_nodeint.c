3204a8fbd0ee2a21bfbe9e3cfd12b2427c28791e
ridhwan497 <ridubangire@gmail.com>
2023-05-03 01:21:51 +0300

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,36 +10,37 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *count; /* count nodes */
-	listint_t *add_node; /* new node */
-	unsigned int index; /* index of the node */
+	listint_t *new_node; /* new node */
+	unsigned int i; /* index of the node */
 
 	if (head == NULL)
 		return (NULL);
 
 	count = *head;
-	for (index = 0; index < idx - 1; index++)
-	{
-		if (count == NULL)
-			return (NULL);
-		count = count->next;
-	}
-
-	add_node = malloc(sizeof(listint_t));
-	if (add_node == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-
-	add_node->n = n;
+	new_node->n = n;
 
 	if (idx == 0)
 	{
-		add_node->next = *head;
-		*head = add_node;
-	}
-	else
-	{
-		add_node->next = count->next;
-		count->next = add_node;
+		new_node->next = count;
+		*head = new_node;
+		return (new_node);
 	}
 
-	return (add_node);
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (count == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		count = count->next;
+	}
+
+	new_node->next = count->next;
+	count->next = new_node;
+
+	return (new_node);
 }
