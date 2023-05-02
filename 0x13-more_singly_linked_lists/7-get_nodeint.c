e82e39a57b829babcc3df6304d77cf41e4cfddba
ridhwan497 <ridubangire@gmail.com>
2023-05-03 01:01:23 +0300

    goet node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node
+ * Return: nth node of a listint_t linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *count; /* count nodes */
+	unsigned int i; /* index of the node */
+
+	if (head == NULL)
+		return (NULL);
+
+	count = head;
+	for (i = 0; i < index; i++)
+	{
+		if (count == NULL)
+			return (NULL);
+		count = count->next;
+	}
+
+	return (count);
+}
