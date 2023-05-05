2d03737f8346bc995918a5203c37d0bf874b3b39
ridhwan497 <ridubangire@gmail.com>
2023-05-05 03:52:11 +0300

    forgot to initialize stuff

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,21 +12,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int count, num;
 
+	count = num = 0;
+
 	if (b == NULL)
 		return (0);
 
 	while (b[count] != '\0')
 	{
-		if (!b[count] == '0' || !b[count] == '1')
-		{
+		if (b[count] != '0' && b[count] != '1')
 			return (0);
-		}
 		num <<= 1; /*here we shift the bits to the left*/
-
 		if (b[count] == '1')
-		{
 			num += 1;
-		}
 		count++;
 	}
 
