d0f086af6c637379c2dabe5221781aa51c7fc616
ridhwan497 <ridubangire@gmail.com>
2023-05-09 00:51:04 +0300

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,14 +22,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		close(f_desc);
-		return (1);
+		text_content = "";
 	}
 
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
 
 	write_count = write(f_desc, text_content, i);
 
